4d77e75f9270e8e77c224f1aa91e8cb1c61b5f0b
Ghofrane Ben Hmaid <54797163+ghofranebenhmaid@users.noreply.github.com>
2020-08-23 18:53:44 +0200

    Add files via upload

---
+++ src/assets/data/logosData.js
@@ -0,0 +1,66 @@
+import logo1 from '../images/logos/logo1-100.jpg';
+import logo2 from '../images/logos/logo2-100.jpg';
+import logo3 from '../images/logos/logo3-100.jpg';
+import logo4 from '../images/logos/logo4-100.jpg';
+import logo5 from '../images/logos/logo5-100.jpg';
+import logo6 from '../images/logos/logo6-100.jpg';
+import logo7 from '../images/logos/logo7-100.jpg';
+
+const logosData = [
+   {
+      id: 1,
+      src: logo1,
+      name: 'NABO SNAK',
+
+      description:
+         'Vivamus finibus lorem sit amet tincidunt finibus. Duis semper consectetur mauris, vitae ultrices felis auctor nec. Nulla interdum sed elit ut tempus. ',
+   },
+   {
+      id: 2,
+      src: logo2,
+      name: 'DIAM WASH',
+      url: 'https://www.behance.net/gallery/75931651/Logo-design-CarWash',
+      description:
+         'Vivamus finibus lorem sit amet tincidunt finibus. Duis semper consectetur mauris, vitae ultrices felis auctor nec. Nulla interdum sed elit ut tempus. ',
+   },
+   {
+      id: 3,
+      src: logo3,
+      name: 'Mettez Votre Empreinte',
+      url:
+         'https://www.behance.net/gallery/75980131/Mettez-Votre-Empreinte-Logo-Design',
+      description:
+         'Vivamus finibus lorem sit amet tincidunt finibus. Duis semper consectetur mauris, vitae ultrices felis auctor nec. Nulla interdum sed elit ut tempus. ',
+   },
+   {
+      id: 4,
+      src: logo4,
+      name: 'TN Plus Tounsi',
+      description:
+         'Vivamus finibus lorem sit amet tincidunt finibus. Duis semper consectetur mauris, vitae ultrices felis auctor nec. Nulla interdum sed elit ut tempus. ',
+   },
+   {
+      id: 4,
+      src: logo5,
+      name: 'ICHKEUL',
+      description:
+         'Vivamus finibus lorem sit amet tincidunt finibus. Duis semper consectetur mauris, vitae ultrices felis auctor nec. Nulla interdum sed elit ut tempus. ',
+   },
+   {
+      id: 4,
+      src: logo6,
+      name: 'logo6',
+      url:
+         'https://www.behance.net/gallery/75984125/_?tracking_source=project_owner_other_projects',
+      description:
+         'Vivamus finibus lorem sit amet tincidunt finibus. Duis semper consectetur mauris, vitae ultrices felis auctor nec. Nulla interdum sed elit ut tempus. ',
+   },
+   {
+      id: 4,
+      src: logo7,
+      name: 'YAWMIA',
+      description:
+         'Vivamus finibus lorem sit amet tincidunt finibus. Duis semper consectetur mauris, vitae ultrices felis auctor nec. Nulla interdum sed elit ut tempus. ',
+   },
+];
+export default logosData;
---
+++ src/assets/data/timelineData.js
@@ -0,0 +1,40 @@
+
+
+const data = [
+   {
+      title:
+         'Photographer / press and media assistant for a member of parliament',
+      date: 'Jan. 2014 – Sep. 2016',
+      location: 'Tunisia',
+      description: [
+         'Management of social media accounts of member of parliament.',
+         'Accompanying member of parliament during all activities. Photography and video recording. Editing graphics and video. Writing and publishing articles online.',
+      ],
+   },
+   {
+      title: 'Magazine graphic designer - Bizerte News',
+      date: 'Okt. 2012 – Nov. 2013',
+      location: 'Tunisia',
+      description: [
+         'Graphic design and layout of texts and images for the whole newspaper as well as frontpage layout.',
+      ],
+   },
+   {
+      title: 'Danish Language 3 - Speak Hellerup',
+      date: 'Jun. 2017 - Present',
+      location: 'Danmark',
+      description: [
+         'Graphic design and layout of texts and images for the whole newspaper as well as frontpage layout.',
+      ],
+   },
+   {
+      title:
+         'Bachelor in IT &  Multimedia - Higher Institute of Multimedia Arts of Manouba',
+      date: 'Sep. 2011 - Jun. 2016',
+      location: 'Tunisia',
+      description: [
+         'Final project: SQL jobs monitor (programmering). Visualising the execution history of SQL Server Agent jobs in graphical mode with the possibility of real time replanning',
+      ],
+   },
+];
+export default data;
